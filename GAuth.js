fbce1a5c842c94b0b0e42ec120d8a39a8d4f55ee
ishakuyusufmaina <75148285+ishakuyusufmaina@users.noreply.github.com>
2023-06-06 23:26:39 +0100

    Add files via upload

---
+++ GAuth.js
@@ -0,0 +1,77 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-analytics.js";
+import {getAuth, signInWithRedirect, getRedirectResult, signOut, onAuthStateChanged, GoogleAuthProvider} from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app-check.js"
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDtm6d00VS-c2BDmWBAllXPGthD8dEb5bo",
+    authDomain: "mainacode.firebaseapp.com",
+    projectId: "mainacode",
+    storageBucket: "mainacode.appspot.com",
+    messagingSenderId: "313458674230",
+    appId: "1:313458674230:web:2cc6d4bd4a2ab68b76e697",
+    measurementId: "G-K0SDZ6Z9CZ"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+
+
+loginPage=`<h1>Sign in</h2>
+<h2>Maina GAuth Demo</h2>
+<p>You are now signed out</p>
+<button onclick="signin()">Sign in with Google</button>`
+
+homePage = `<h1>Home Page</h2>
+<h2>Maina Gauth Demo</h2>
+<p>You are now signed in as <span id="user"></span></p>
+<button onclick ="signout()">Sign out</button>`
+
+
+let main = document.getElementById("main");
+
+if(localStorage.getItem("user")){
+    main.innerHTML = homePage;
+    document.getElementById("user").innerHTML = "user: " + localStorage.getItem("user");
+}
+else {
+    main.innerHTML = loginPage;
+}
+
+function signin(){
+    signInWithRedirect(auth, provider)
+}
+
+getRedirectResult(auth).then((result)=>{
+    alert("sucessfully signed in")
+}).catch((error)=>{
+    alert("error: " + error)
+})
+
+function signout(){
+    signOut(auth).then(()=>{
+        alert("Successfully signed out")
+    }).catch((error)=>{
+        alert(error)
+    })
+}
+
+onAuthStateChanged(auth, (user)=>{
+    if (user){
+        main.innerHTML = homePage;
+        document.getElementById("user").innerHTML = "user: " + user;
+        localStorage.setItem("user", user);
+    }
+    else {
+        localStorage.removeItem("user");
+        main.innerHTML = loginPage;
+    }
+})
+
